df69c4c0d5c6e513f7e2c4e2bc697feaa9b31788
Theseregrets <rishitashaw49@gmail.com>
2021-05-25 18:45:46 +0530

    styling added

--- Components/Coins/index.jsx
+++ Components/Coins/index.jsx
@@ -1,4 +1,4 @@
-import "./coin.module.css";
+import styles from "./coin.module.css";
 
 export default function Coin({
   name,
@@ -11,20 +11,24 @@ export default function Coin({
   price_change_percentage_24h,
 }) {
   return (
-    <div className="coin_container">
-      <div className="coin_row">
-        <div className="coin">
-          <img src={src} alt={name} />
-          <h1 className="coin_h1">{name}</h1>
-          <p className="coin_symbol">{symbol}</p>
+    <div className={styles.coin_container}>
+      <div className={styles.coin_row}>
+        <div className={styles.coin}>
+          <img src={src} alt={name} className={styles.coin_img} />
+          <h1 className={styles.coin_h1}>{name}</h1>
+          <p className={styles.coin_symbol}>{symbol}</p>
         </div>
-        <div className="coin_data">
-          <p className="coin_price">₹ {current_price}</p>
-          <p className="coin_volume">₹ {total_volume.toLocaleString()}</p>
+        <div className={styles.coin_data}>
+          <p className={styles.coin_price}>₹ {current_price}</p>
+          <p className={styles.coin_volume}>
+            ₹ {total_volume.toLocaleString()}
+          </p>
           {price_change_24h < 0 ? (
-            <p className="coin_percent red">{price_change_percentage_24h} %</p>
+            <p className={(styles.coin_percent, styles.red)}>
+              {price_change_percentage_24h} %
+            </p>
           ) : (
-            <p className="coin_percent green">
+            <p className={(styles.coin_percent, styles.green)}>
               {price_change_percentage_24h} %
             </p>
           )}
